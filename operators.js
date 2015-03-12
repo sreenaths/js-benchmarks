f18135cfa068b91776bb726b223b6f37163b526b
sreenaths <somarajapuram@gmail.com>
2015-03-12 13:49:15 +0530

    Checking division by two in 3 ways

---
+++ operators.js
@@ -0,0 +1,42 @@
+
+/**
+ * Js Benchmarks
+ *
+ * Benchmarking for opeartors.
+ */
+
+function test(title, testFunction) {
+  var startTime = Date.now();
+  testFunction();
+  console.log(title, ":", Date.now() - startTime, "Milliseconds");
+  return '--- Test End ---';
+}
+
+var ITERATIONS = 1000000;
+
+test("Division by 2 : Using /", function () {
+
+  var a = 4, b;
+  for(var i = 0; i < ITERATIONS; i++) {
+    b = a / 2;
+  }
+
+});
+
+test("Division by 2 : Using *", function () {
+
+  var a = 4, b;
+  for(var i = 0; i < ITERATIONS; i++) {
+    b = a * .5;
+  }
+
+});
+
+test("Division by 2 : Using >>", function () {
+
+  var a = 4, b;
+  for(var i = 0; i < ITERATIONS; i++) {
+    b = a >> 1;
+  }
+
+});
